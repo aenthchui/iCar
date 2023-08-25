3f3236d0a9405d24e2144c14ea37a8a08a51384b
aenthchui <96768439+aenthchui@users.noreply.github.com>
2023-08-25 18:08:18 +0800

    Update experiment.ts

--- experiment.ts
+++ experiment.ts
@@ -323,6 +323,18 @@ namespace CUHK_JC_iCar_Experiments {
                 Line_Follow_Until_Tag(Target, LSpeed, RSpeed, FSpeed, true)
                 CUHK_JC_iCar.headLightsOff()
                 while (tag.length != 0) {
+                    if(Current_Location == 3 && Target == 5){
+                        Line_Follow_Until_Tag(4, LSpeed, RSpeed, FSpeed, false)
+                        while (CUHK_JC_iCar.Line_Sensor(CUHK_JC_iCar.enPos.Left, CUHK_JC_iCar.enLineState.WhiteLine)) {
+                            CUHK_JC_iCar.carCtrlSpeed(CUHK_JC_iCar.CarState.TurnRight, RSpeed)
+                        }
+                    }
+                    if(Current_Location == 5 && Target == 7){
+                        Line_Follow_Until_Tag(6, LSpeed, RSpeed, FSpeed, false)
+                        while (CUHK_JC_iCar.Line_Sensor(CUHK_JC_iCar.enPos.Left, CUHK_JC_iCar.enLineState.WhiteLine)) {
+                            CUHK_JC_iCar.carCtrlSpeed(CUHK_JC_iCar.CarState.TurnRight, RSpeed)
+                        }
+                    }
                     if (tag[0] - Target <= 2) {
                         Target = tag.shift()
                         Line_Follow_Until_Tag(Target, LSpeed, RSpeed, FSpeed, false)
@@ -341,19 +353,6 @@ namespace CUHK_JC_iCar_Experiments {
                             CUHK_JC_iCar.carCtrlSpeed(CUHK_JC_iCar.CarState.TurnRight, RSpeed)
                         }
                     }
-                    if(Current_Location == 3 && Target == 5){
-                        Line_Follow_Until_Tag(4, LSpeed, RSpeed, FSpeed, false)
-                        while (CUHK_JC_iCar.Line_Sensor(CUHK_JC_iCar.enPos.Left, CUHK_JC_iCar.enLineState.WhiteLine)) {
-                            CUHK_JC_iCar.carCtrlSpeed(CUHK_JC_iCar.CarState.TurnRight, RSpeed)
-                        }
-                    }
-                    if(Current_Location == 5 && Target == 7){
-                        Line_Follow_Until_Tag(6, LSpeed, RSpeed, FSpeed, false)
-                        while (CUHK_JC_iCar.Line_Sensor(CUHK_JC_iCar.enPos.Left, CUHK_JC_iCar.enLineState.WhiteLine)) {
-                            CUHK_JC_iCar.carCtrlSpeed(CUHK_JC_iCar.CarState.TurnRight, RSpeed)
-                        }
-                    }
-                    
                 }
                 while (CUHK_JC_iCar.Line_Sensor(CUHK_JC_iCar.enPos.Left, CUHK_JC_iCar.enLineState.BlackLine)) {
                     CUHK_JC_iCar.carCtrlSpeed(CUHK_JC_iCar.CarState.SpinLeft, LSpeed)
